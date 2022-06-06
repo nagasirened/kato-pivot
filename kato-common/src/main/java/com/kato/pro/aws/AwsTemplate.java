3a8299d5767e72e256e63d5f08c90fc50bab2443
zengguangfu <katouyi@foxmail.com>
2022-06-06 16:05:06 +0800

    delete unuseful dependency

--- kato-common/src/main/java/com/kato/pro/aws/AwsTemplate.java
+++ kato-common/src/main/java/com/kato/pro/aws/AwsTemplate.java
@@ -1,42 +1,31 @@
 package com.kato.pro.aws;
 
-import cn.hutool.core.date.DateTime;
-import cn.hutool.core.date.DateUtil;
-import cn.hutool.core.util.StrUtil;
 import com.amazonaws.services.s3.model.*;
-import com.amazonaws.util.IOUtils;
-import lombok.Cleanup;
-import lombok.SneakyThrows;
-
-import java.io.ByteArrayInputStream;
 import java.io.InputStream;
-import java.net.URL;
-import java.util.Date;
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
 
 public interface AwsTemplate {
 
-    public List<S3ObjectSummary> getObjectsByPrefix(String bucketName, String prefix);
+     List<S3ObjectSummary> getObjectsByPrefix(String bucketName, String prefix);
 
-    public String getObjectUrl(String bucketName, String objectName, Integer expires);
+     String getObjectUrl(String bucketName, String objectName, Integer expires);
 
-    public S3Object getObject(String bucketName, String objectName);
+     S3Object getObject(String bucketName, String objectName);
 
-    public InputStream getObjectInputStream(String bucketName, String objectName);
+     InputStream getObjectInputStream(String bucketName, String objectName);
 
-    public void uploadObject(String bucketName, String objectName, InputStream inputStream);
+     void uploadObject(String bucketName, String objectName, InputStream inputStream);
 
-    public PutObjectResult uploadObject(String bucketName, String objectName, InputStream inputStream, long size, String contentType);
+     PutObjectResult uploadObject(String bucketName, String objectName, InputStream inputStream, long size, String contentType);
 
-    public void removeObject(String bucketName, String objectName);
+     void removeObject(String bucketName, String objectName);
 
-    public void createBucket(String bucketName);
+     void createBucket(String bucketName);
 
-    public List<Bucket> getAllBucket();
+     List<Bucket> getAllBucket();
 
-    public Optional<Bucket> getBucketByName(String bucketName);
+     Optional<Bucket> getBucketByName(String bucketName);
 
-    public void removeBucket(String bucketName);
+     void removeBucket(String bucketName);
 }
