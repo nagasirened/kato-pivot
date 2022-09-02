d58c67ab03c1d0d582f1e2d70f3a0556d3468814
zengguangfu <katouyi@foxmail.com>
2022-09-02 17:56:50 +0800

    baseAuth

---
+++ kato-core/kato-base/src/main/java/com/kato/pro/thread/UserContextHolder.java
@@ -0,0 +1,17 @@
+package com.kato.pro.thread;
+
+import com.kato.pro.entity.LoginUser;
+
+public class UserContextHolder {
+
+    private static ThreadLocal<LoginUser> userHolder = new ThreadLocal<>();
+
+    public static LoginUser getUser() {
+        return userHolder.get();
+    }
+
+    public static void setUser(LoginUser loginUser) {
+        userHolder.set(loginUser);
+    }
+
+}
