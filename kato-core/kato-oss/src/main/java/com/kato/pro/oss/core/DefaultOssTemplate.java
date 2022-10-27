fd9d3638aa3d5bc91366cd62b50f667eb0e1dbaf
zengguangfu <katouyi@foxmail.com>
2022-10-27 13:59:57 +0800

    OssTemplate rollback

--- kato-core/kato-oss/src/main/java/com/kato/pro/oss/core/DefaultOssTemplate.java
+++ kato-core/kato-oss/src/main/java/com/kato/pro/oss/core/DefaultOssTemplate.java
@@ -8,59 +8,120 @@ import java.io.InputStream;
 import java.net.URL;
 import java.util.Date;
 import java.util.List;
-import java.util.Optional;
-import java.util.function.Function;
 
 @Slf4j
 public class DefaultOssTemplate implements OssTemplate{
 
-    private final OSS client;
+    private final OssLinksPoolUtils ossLinksPoolUtils;
 
-    public DefaultOssTemplate(OSS ossClient) {
-        this.client = ossClient;
+    public DefaultOssTemplate(OssLinksPoolUtils ossLinksPoolUtils) {
+        this.ossLinksPoolUtils = ossLinksPoolUtils;
+    }
+
+    private OSS getClient() {
+        OSS ossClient = ossLinksPoolUtils.getOssClient();
+        if (ossClient == null) {
+            log.error("get ossClient fail");
+            throw new RuntimeException("system error");
+        }
+        return ossClient;
+    }
+
+    private void releaseClient(OSS ossClient) {
+        ossLinksPoolUtils.returnOssClient(ossClient);
     }
 
     public ListObjectsV2Result getObjectsByPrefix(String bucketName, String prefix) {
-        return client.listObjectsV2(bucketName, prefix);
+        OSS client = getClient();
+        try {
+            return client.listObjectsV2(bucketName, prefix);
+        } finally {
+            releaseClient(client);
+        }
     }
 
     public URL getObjectUrl(String bucketName, String objectName, Date expires) {
-        return client.generatePresignedUrl(bucketName, objectName, expires);
+        OSS client = getClient();
+        try {
+            return client.generatePresignedUrl(bucketName, objectName, expires);
+        } finally {
+            releaseClient(client);
+        }
     }
 
     public PutObjectResult uploadObject(String bucketName, String objectName, InputStream inputStream) {
-        return client.putObject(bucketName, objectName, inputStream);
+        OSS client = getClient();
+        try {
+            return client.putObject(bucketName, objectName, inputStream);
+        } finally {
+            releaseClient(client);
+        }
     }
 
     public PutObjectResult uploadObject(String bucketName, String objectName, InputStream inputStream, long size, String contentType) {
-        ObjectMetadata objectMetadata = new ObjectMetadata();
-        objectMetadata.setContentType(contentType);
-        objectMetadata.setContentLength(size);
-        return client.putObject(bucketName, objectName, inputStream, objectMetadata);
+        OSS client = getClient();
+        try {
+            ObjectMetadata objectMetadata = new ObjectMetadata();
+            objectMetadata.setContentType(contentType);
+            objectMetadata.setContentLength(size);
+            return client.putObject(bucketName, objectName, inputStream, objectMetadata);
+        } finally {
+            releaseClient(client);
+        }
     }
 
     public void removeObject(String bucketName, String objectName) {
-        client.deleteObject(bucketName, objectName);
+        OSS client = getClient();
+        try {
+            client.deleteObject(bucketName, objectName);
+        } finally {
+            releaseClient(client);
+        }
     }
 
     public void createBucket(String bucketName) {
-        client.createBucket(bucketName);
+        OSS client = getClient();
+        try {
+            client.createBucket(bucketName);
+        } finally {
+            releaseClient(client);
+        }
     }
 
     public List<Bucket> getAllBucket() {
-        return client.listBuckets();
+        OSS client = getClient();
+        try {
+            return client.listBuckets();
+        } finally {
+            releaseClient(client);
+        }
     }
 
     public BucketInfo getBucketByName(String bucketName) {
-        return client.getBucketInfo(bucketName);
+        OSS client = getClient();
+        try {
+            return client.getBucketInfo(bucketName);
+        } finally {
+            releaseClient(client);
+        }
     }
 
     public void removeBucket(String bucketName) {
-        client.deleteBucket(bucketName);
+        OSS client = getClient();
+        try {
+            client.deleteBucket(bucketName);
+        } finally {
+            releaseClient(client);
+        }
     }
 
     public OSSObject getObject(String bucketName, String name) {
-        return client.getObject(bucketName, name);
+        OSS client = getClient();
+        try {
+            return client.getObject(bucketName, name);
+        } finally {
+            ossLinksPoolUtils.returnOssClient(client);
+        }
     }
 
 }
